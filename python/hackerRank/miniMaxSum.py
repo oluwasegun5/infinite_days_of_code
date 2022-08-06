8cd241a70f2624dd19c77d23006ecffe3b340886
oluwasegun5 <adeayosegun5@gmail.com>
2022-08-06 21:17:59 +0100

    miniMaxSum

---
+++ python/hackerRank/miniMaxSum.py
@@ -0,0 +1,14 @@
+def miniMaxSum(arr):
+    # Write your code here
+    lowest_number = 10000000000
+    highest_number = 0
+    sum = 0
+
+    for i in range(len(arr)):
+        current_number = arr[i]
+        if current_number > highest_number:
+            highest_number = current_number
+        if current_number < lowest_number:
+            lowest_number = current_number
+        sum += current_number
+    print(f'{sum - highest_number} {sum - lowest_number}')
